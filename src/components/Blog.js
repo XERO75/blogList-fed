dcdbde8ed64ccf24efdf585155944072a4f6a05a
XERO75 <xerolry@gmail.com>
2021-04-12 23:23:18 +0800

    ud

--- src/components/Blog.js
+++ src/components/Blog.js
@@ -1,12 +1,29 @@
 import React from 'react';
-const Blog = ({ blogs }) => (
-  <div>
+import Togglable from './Togglable';
+
+const Blog = ({ blogs }) => {
+  const blogStyle = {
+    paddingTop: 10,
+    paddingLeft: 2,
+    border: 'solid',
+    borderWidth: 1,
+    marginBottom: 5
+  }
+  return (
+  <div >
     {blogs.map((blog) => (
-      <div key={blog.id}>
+      <div key={blog.id} style={blogStyle}>
         {blog.title} {blog.author}
+        <Togglable buttonLabel="view" >
+          <div>{blog.url}</div> 
+          <div>{blog.likes}</div> 
+          <div>{blog.author}</div> 
+        </Togglable>
       </div>
     ))}
   </div>
-);
+  )
+}
+  
 
 export default Blog;
